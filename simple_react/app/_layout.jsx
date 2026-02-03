020a5deeba0810bce237da36ebd05edc1cbc93d7
vanilasky00 <pipichun2@gmail.com>
2026-02-03 17:10:57 +0800

    (feat) Add tab navigation system

--- simple_react/app/_layout.jsx
+++ simple_react/app/_layout.jsx
@@ -16,6 +16,7 @@ const RootLayout = () => {
             }}>
                 <Stack.Screen name='(auth)' options={{ headerShown: false }}/>
                 <Stack.Screen name='index' options={{ title: 'Home'}}/>
+                <Stack.Screen name='(dashboard)' options={{ headerShown: false }}/>
             </Stack> 
         </>
     )
